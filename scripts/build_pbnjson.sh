fa2a99a41811470dbee3ada926da472be9442e37
Randall E. Barker <randall.barker@palm.com>
2012-04-04 10:55:34 -0700

    Build release version of json libs to work around assert crash.

--- scripts/build_pbnjson.sh
+++ scripts/build_pbnjson.sh
@@ -9,12 +9,12 @@ export MY_YAJL_INCLUDE_PATH=$ISIS_ROOT/pbnjson/src/deps/yajl/yajl-1.0.7/include
 export MY_YAJL_LIB_PATH=$ISIS_ROOT/pbnjson/src/deps/yajl/yajl-1.0.7/lib
 
 cd $ISIS_ROOT/$NAME/src/deps/yajl
-cmake . -DCMAKE_INSTALL_PREFIX=$LUNA_STAGING
+cmake . -DCMAKE_INSTALL_PREFIX=$LUNA_STAGING -DCMAKE_BUILD_TYPE=RELEASE
 make -j$PROCCOUNT
 make install
 
 cd $ISIS_ROOT/$NAME/src
-cmake . -DCMAKE_INSTALL_PREFIX=$LUNA_STAGING
+cmake . -DCMAKE_INSTALL_PREFIX=$LUNA_STAGING -DCMAKE_BUILD_TYPE=RELEASE
 make -j$PROCCOUNT
 make install
 
