379905602b7ebd18196566765dd515b2d27beac3
Stuart Eichert <stuart.eichert@palm.com>
2012-07-05 11:06:07 -0700

    Cleanup qtsetup.sh - Remove commented out code, verbosity

--- scripts/common/qtsetup.sh
+++ scripts/common/qtsetup.sh
@@ -3,21 +3,6 @@
 export QTDIR=$ISIS_ROOT/qt
 QMAKE=$LUNA_STAGING/bin/qmake
 
-#if [ -z "$QTDIR" ] ; then
-#   BUILD_QT=true
-#fi
-
-#build_qt()
-#{
-#   [ -z "${BUILD_QT}" ] && return 1 || return 0
-#}
-
-qt_clone()
-{
-   git clone -b 4.8 https://git.gitorious.org/qt/qt.git $ISIS_ROOT/qt
-#   [ "$?" == "0" ] || echo "Failed to checkout: qt"
-}
-
 qt_download()
 {
    local QT_FTP="ftp://ftp.qt.nokia.com/qt/source"
@@ -28,7 +13,8 @@ qt_download()
    curl -C - $QT_FTP/$QT_TGZ -o $ISIS_ROOT/$QT_TGZ
    [ "$?" == "0" ] || fail "Failed to download: $QT_TGZ"
 
-   tar xzvf $ISIS_ROOT/$QT_TGZ -C $ISIS_ROOT
+   echo Extracting $QT_TGZ
+   tar xzf $ISIS_ROOT/$QT_TGZ -C $ISIS_ROOT
    [ "$?" == "0" ] || fail "Failed to untar: $QT_TGZ"
 
    mv $ISIS_ROOT/$QT_SRC $ISIS_ROOT/qt
@@ -38,8 +24,6 @@ qt_download()
 get_qt()
 {
    if [ ! -d $ISIS_ROOT/qt ] ; then
-#      qt_clone
-#      [ "$?" == "0" ] || qt_download
       qt_download
    else
       echo found $ISIS_ROOT/qt
