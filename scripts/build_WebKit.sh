dbe96919f45efdd205242b58559ee28c4ac9b5c9
Scott Shillcock <scott.shillcock@palm.com>
2012-07-05 14:09:23 -0700

    Update build_WebKit.sh to use build-webkit
    build_WebKit.sh was calling qmake directly, instead use the build
    script provided with webkit.

--- scripts/build_WebKit.sh
+++ scripts/build_WebKit.sh
@@ -10,37 +10,33 @@ PROCCOUNT=$2
 cd $ISIS_ROOT/$NAME
 
 export QMAKEPATH=$ISIS_ROOT/WebKit/Tools/qmake
+export WEBKITOUTPUTDIR="WebKitBuild/isis-x86"
 
-mkdir -p WebKitBuild/isis-x86/Release
-pushd WebKitBuild/isis-x86/Release
-
-$QMAKE ../../../$NAME.pro -o Makefile.$NAME DEFINES+=XP_UNIX DEFINES+=XP_WEBOS DEFINES+=PALM_DEVICE DEFINES+=MACHINE_DESKTOP DEFINES+=QT_WEBOS DEFINES+=ENABLE_VIDEO=1 DEFINES+=WTF_USE_GSTREAMER=1 DEFINES+=ENABLE_GLIB_SUPPORT=1 CONFIG+=release
-
-if [ "$?" != "0" ] ; then
-   echo Failed to qmake $NAME
-   exit 1
-fi
-
-make -f Makefile.$NAME -j$PROCCOUNT
+./Tools/Scripts/build-webkit --qt \
+--release \
+--video \
+--no-webgl \
+--no-webkit2 \
+--qmake="${QMAKE}" \
+--makeargs="-j$PROCOUNT" \
+--qmakearg="DEFINES+=XP_UNIX" \
+--qmakearg="DEFINES+=XP_WEBOS" \
+--qmakearg="DEFINES+=PALM_DEVICE" \
+--qmakearg="DEFINES+=MACHINE_DESKTOP" \
+--qmakearg="DEFINES+=QT_WEBOS" \
+--qmakearg="DEFINES+=WTF_USE_ZLIB=1" \
+--qmakearg="DEFINES+=WTF_USE_GSTREAMER=1" \
+--qmakearg="DEFINES+=ENABLE_GLIB_SUPPORT=1"
 
 if [ "$?" != "0" ] ; then
    echo Failed to make $NAME
    exit 1
 fi
 
-mkdir -p $LUNA_STAGING/imports
-cp -p -R imports/QtWebKit $LUNA_STAGING/imports/
-cp lib/libQtWebKit.so* $LUNA_STAGING/lib
-cp -R include/QtWebKit $LUNA_STAGING/include
-cd include/QtWebKit
-for FILE in *.h ; do
-   echo $FILE
-   cp $ISIS_ROOT/$NAME/Source/WebKit/qt/Api/$FILE $LUNA_STAGING/include/QtWebKit/$FILE
-done
-
-popd
-if [ ! -z "$PACKAGE" ]; then
-    cd $ISIS_ROOT/$NAME
-    dpkg-buildpackage -rfakeroot
-    cd $STARTDIR
+pushd $WEBKITOUTPUTDIR/Release
+make install
+if [ "$?" != "0" ] ; then
+   echo Failed to install $NAME
+   exit 1
 fi
+popd
